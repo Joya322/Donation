feb12aac365ce0b2a280be79999ee71931427074
Joya322 <jannatferdoush1212@gmail.com>
2025-08-20 14:26:40 +0600

    donate now btn alert added and 100% completed

--- assets/js/donationCalculation.js
+++ assets/js/donationCalculation.js
@@ -8,28 +8,28 @@ donateNowBtns.forEach((btn) => {
     // getting total balance amount of donation
     let totalBalanceElement = findElementByID("balanceAmount");
     let totalBalance = totalBalanceElement.innerText;
-
+    
     // cache the cardContents div/parent of the clicked button
     const parentDiv = btn.parentNode;
-
+    
     // getting the input field and value
     const inputFieldText = parentDiv.querySelector(".donationInput");
     const inputFieldValue = getInputFieldValue(inputFieldText);
-
+    
     // getting previous donationBalance
     const donationBalanceElement = parentDiv.querySelector(".donationBalance");
     const donationBalance = donationBalanceElement.innerText;
-
+    
     // convert string values to numerical values
     if (isNaN(parseFloat(inputFieldValue))) {
       alert("Invalid Amount");
       return;
     }
-
+    
     const inputAmount = parseFloat(inputFieldValue);
     let donationBalanceAmount = parseFloat(donationBalance);
     let totalBalanceAmount = parseFloat(totalBalance);
-
+    
     // set new value
     if (inputAmount < 0) {
       alert("Invalid Input");
@@ -37,12 +37,12 @@ donateNowBtns.forEach((btn) => {
     }
     totalBalanceAmount -= inputAmount;
     donationBalanceAmount += inputAmount;
-
+    
     // set new value to the elements
     totalBalanceElement.innerText = totalBalanceAmount;
-
+    
     donationBalanceElement.innerText = donationBalanceAmount;
-
+    
     // ***setting donation history***
     // create div element
     let div = document.createElement("div");
@@ -57,7 +57,7 @@ donateNowBtns.forEach((btn) => {
       "px-8",
       "py-3"
     );
-
+    
     // create title h3
     let h3 = document.createElement("h3");
     h3.classList.add(
@@ -66,25 +66,26 @@ donateNowBtns.forEach((btn) => {
       "text-xl",
       "text-justify"
     );
-
+    
     // set innerText of h3
     let reason = btn.getAttribute("data-target");
     h3.innerText = `${inputAmount} Taka is Donated for famine-2024 at ${reason}, Bangladesh`;
-
+    
     // create time history tag p
     let p = document.createElement("p");
     p.classList.add("font-extralight", "opacity-70", "text-justify");
-
+    
     // get real time date and time
     const now = new Date();
-
+    
     // set innerText of p
     p.innerText = `Date : ${now}`;
-
+    
     // getting parent div to history adding
     let historyTab = document.getElementById("historyTab");
     div.append(h3, p);
     historyTab.appendChild(div);
     console.log(historyTab);
+    alert("Congrats! Successfully donated");
   });
 });
